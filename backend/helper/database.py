404ac3557084f6401d9e691d8366b74d191776a9
abhishek <abhishekbhatt095@gmail.com>
2026-06-11 21:43:35 +0530

    updated databse.py

--- backend/helper/database.py
+++ backend/helper/database.py
@@ -1,32 +1,9 @@
-import psycopg2
 import os
 from dotenv import load_dotenv
+import psycopg2
+
 load_dotenv()
 
-
-def get_connection():
-    return psycopg2.connect(
-        host="localhost",
-        database="RAAG",
-        user="postgres",
-        password="Abhishek",
-        port="5433"
-    )
-
-def get_users_connection():
-    return psycopg2.connect(
-        host="localhost",
-        database="users",
-        user="postgres",
-        password="Abhishek",
-        port = "5433"
-    )
-
-def add_product():
-    return psycopg2.connect(
-        host="localhost",
-        database="product",
-        user="postgres",
-        password="Abhishek",
-        port = "5433"
-    )
+conn = psycopg2.connect(
+    os.getenv("POSTGRES_URL")
+)
